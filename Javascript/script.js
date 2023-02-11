28b6f97cd1e10d2dc9537fa1ba7c58e6bb6aae49
Michael-Mueller-Vienna <michael.mueller.bf@gmail.com>
2023-02-11 13:29:15 +0100

    Step 4 Addition js file

---
+++ Javascript/script.js
@@ -0,0 +1,37 @@
+let = tasks [
+    { 
+"taskName": "shopping", 
+"image": "img/groceries.jpg", 
+"description": "Shop for weekly groceries at Billa and Hofer.", 
+"importance": 3
+},
+
+{
+"taskName": "cleaning",
+"image": "img/cleaning.jpg",
+"description": "Do the general cleaning of the house - vacuum, mop the floor, clean the bathroom and kitchen.",
+"importance": 1
+},
+
+{
+"taskName": "laundry",
+"image": "img/laundry.jpg",
+"description": "Wash and dry all the dirty clothes in the house.",
+"importance": 4
+},
+
+{
+"taskName": "cooking",
+"image": "img/cooking.jpg",
+"description": "Prepare dinner for the family.",
+"importance": 2
+},
+
+{
+"taskName": "exercise",
+"image": "img/exercise.jpg",
+"description": "Go for a 30-minute run in the park.",
+"importance": 1
+}
+];
+
