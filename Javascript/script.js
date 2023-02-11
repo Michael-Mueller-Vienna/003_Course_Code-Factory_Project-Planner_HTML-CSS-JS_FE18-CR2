fadbe286f7b099bf335357d1a6b12d99182ce479
Michael-Mueller-Vienna <michael.mueller.bf@gmail.com>
2023-02-11 15:50:53 +0100

    Step 6 Counter and Dokumentation

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,32 +1,36 @@
+//Data
+
 let = tasks [{ 
 taskName: "shopping", 
-image: "img/groceries.jpg", 
+image: "https://cdn.pixabay.com/photo/2016/04/03/11/36/berlin-1304370_1280.jpg", 
 description: "Shop for weekly groceries at Billa and Hofer.", 
 importance: 3
 },{
 taskName: "cleaning",
-image: "img/cleaning.jpg",
+image: "https://cdn.pixabay.com/photo/2020/02/25/14/40/chore-4879062_1280.jpg",
 description: "Do the general cleaning of the house - vacuum, mop the floor, clean the bathroom and kitchen.",
 importance: 1
 },{
 taskName: "laundry",
-image: "img/laundry.jpg",
+image: "https://cdn.pixabay.com/photo/2015/06/10/13/23/clothesline-804812_1280.jpg",
 description: "Wash and dry all the dirty clothes in the house.",
 importance: 4
 },{
 taskName: "cooking",
-image: "img/cooking.jpg",
+image: "https://cdn.pixabay.com/photo/2017/02/15/10/39/salad-2068220_1280.jpg",
 description: "Prepare dinner for the family.",
 importance: 2
 },{
 taskName: "exercise",
-image: "img/exercise.jpg",
+image: "https://cdn.pixabay.com/photo/2017/08/06/12/52/woman-2592247_1280.jpg",
 description: "Go for a 30-minute run in the park.",
 importance: 1
 }
 ];
 
-for (let val of people){
+//Creation Cards
+
+for (let val of tasks){
     document.getElementById("result").innerHTML += `
     <div>
     <div class="card" style="width: 18rem;">
@@ -35,9 +39,22 @@ for (let val of people){
     <h5 class="card-title">${val.taskName}</h5>
     <p class="likes">${val.importance}</p>
     <p class="bin btn-success likeBtn">Like</p>
-    <p class="btn btn-warning showMore">Show more info</p>
+    <p class="card-text">${val.description}</p>
     </div>
     </div>
     </div>`
     }
     
+//Counter
+
+let btns = document.getElementsByClassName("likeBtn");
+
+for(let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click",function(){
+        tasks[i].like++;
+        console.log(people[i].like);
+        document.getElementsByClassName("likes")[i].innerHTML = tasks[i].like;
+    })
+}
+
+//
